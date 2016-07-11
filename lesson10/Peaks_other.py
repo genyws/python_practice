558f0baeb2f0f7c9ac3570b812f3364958cb1216
genyws <genyws@gmail.com>
2016-07-11 17:10:00 +0900

    solution for codility lessons 10 : peaks (wrote from other... OTL)

---
+++ lesson10/Peaks_other.py
@@ -0,0 +1,59 @@
+from math import sqrt
+
+def solution(A):
+    A_len = len(A)
+    peaks_until_here = [0]*A_len
+
+    # Retrieve how many peaks exist from beginning to current
+    # position.
+    for index in range(1, A_len-1):
+        peaks_until_here[index] = peaks_until_here[index-1]
+        if A[index] > A[index-1] and A[index] > A[index+1]:
+            peaks_until_here[index] += 1
+    if A_len < 3 or peaks_until_here[-2] == 0:
+        # The array is too short to have a peak. OR
+        # There is no peak in this array.
+        return 0
+    peaks_until_here[-1] = peaks_until_here[-2]
+
+    max_blocks = 0
+    # Compute every possible partition plan, and find out the
+    # one with most blocks.
+    for candidate in range(1, int(sqrt(A_len))+1, 1):
+        if A_len % candidate == 0:
+            blocks, block_size = candidate, A_len//candidate
+
+            # Check the first block.
+            if peaks_until_here[0] < peaks_until_here[block_size-1]:
+                # Check the following blocks.
+                for each_block in range(block_size, A_len, block_size):
+                    if peaks_until_here[each_block-1] == peaks_until_here[each_block+block_size-1]:
+                        # No peak is found in the next block
+                        # This partition plan is not accepted
+                        break
+                else:
+                    max_blocks = blocks
+
+            if candidate * candidate == A_len:
+                # If candidate is equal to sqrt(A_len) exactly,
+                # candidate would equal to A_len//candidate.
+                continue
+
+            block_size, blocks = candidate, A_len//candidate
+            # Check the first block.
+            if peaks_until_here[0] < peaks_until_here[block_size-1]:
+                # Check the following blocks.
+                for each_block in range(block_size, A_len, block_size):
+                    if peaks_until_here[each_block-1] == peaks_until_here[each_block+block_size-1]:
+                        # No peak is found in the next block
+                        # This partition plan is not accepted
+                        break
+                else:
+                    return blocks
+
+    return max_blocks
+
+
+if __name__ == '__main__':
+    print(solution([1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]))
+    print(solution([1, 2, 3, 4, 5, 6]))
